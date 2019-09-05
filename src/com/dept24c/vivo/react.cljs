6370dddcdab78a68cc8927481c9df9b23701f99a
Chad Harrington <chad@deercreeklabs.com>
2019-09-05 15:26:29 -0600

    Add use-on-outside-click custom hook.

--- src/com/dept24c/vivo/react.cljs
+++ src/com/dept24c/vivo/react.cljs
@@ -3,7 +3,7 @@
    ["react" :as React]
    ["react-dom/server" :as ReactDOMServer]
    [com.dept24c.vivo.utils :as u]
-   [oops.core :refer [oapply ocall]]))
+   [oops.core :refer [oapply ocall oget]]))
 
 (defn create-element [& args]
   (oapply React :createElement args))
@@ -22,11 +22,34 @@
      (ocall React :useEffect effect dependencies)
      (ocall React :useEffect effect))))
 
-(defn use-ref [initial-value]
-  (ocall React :useRef initial-value))
+(defn use-ref
+  ([]
+   (use-ref nil))
+  ([initial-value]
+   (ocall React :useRef initial-value)))
 
 (defn use-state [initial-state]
   (ocall React :useState initial-state))
 
 (defn with-key [element k]
-  (ocall React :cloneElement element (clj->js {"key" k})))
+  (ocall React :cloneElement element #js {"key" k}))
+
+;; Custom Hooks
+
+(defn use-on-outside-click
+  "Calls the given callback when a click outside the referenced element.
+   Returns a reference which should be added as a `ref` property to the
+   referenced element, e.g. `{:ref (react/use-on-outside-click close-menu)}"
+  [cb]
+  (let [el-ref (use-ref)
+        handle-outside-click (fn [e]
+                               (when-not (ocall el-ref "current.contains"
+                                                (oget e :target))
+                                 (cb)))
+        effect (fn []
+                 (ocall js/document :addEventListener "mousedown"
+                        handle-outside-click)
+                 #(ocall js/document :removeEventListener "mousedown"
+                         handle-outside-click))]
+    (use-effect effect #js [])
+    el-ref))
